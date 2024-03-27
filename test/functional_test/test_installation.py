31542404ff748508672faeadef540e42e401971d
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-27 10:05:34 +0530

    Trying pytest

---
+++ test/functional_test/test_installation.py
@@ -0,0 +1,8 @@
+def test_pkg_installation():
+    try:
+        import awesome_package
+    except Exception as e:
+        assert False, f"Error: {e}. Awesome_package is not installed correctly."
+        
+    # with pytest.raises(ImportError):
+    #     import Awesome_package
