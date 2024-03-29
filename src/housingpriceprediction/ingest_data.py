3cf51bf11c1b5e0d5492785f354f9901290530f8
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-29 20:00:15 +0530

    Fixed path in ingest_data for housing_path

--- src/housingpriceprediction/ingest_data.py
+++ src/housingpriceprediction/ingest_data.py
@@ -11,14 +11,16 @@ DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("data", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "data/housing/housing.tgz"
 
-
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    os.makedirs(housing_path, exist_ok=True)
+    os.makedirs(housing_path, exist_ok=True)  
     tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+# Rest of the code remains the same
+
 
 
 def load_housing_data(housing_path=HOUSING_PATH):
