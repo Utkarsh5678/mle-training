633ecdf18140cc874d7166574ecb2a9c5db87941
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-29 09:43:38 +0530

    artifact14

--- test/functional_test/test_installation.py
+++ test/functional_test/test_installation.py
@@ -1,20 +1,20 @@
 def test_pkg_installation():
     try:
-        import models
+        import mypackage
     except Exception as e:
         assert False, f"Error: {e}. Awesome_package is not installed correctly."
 
     try:
-        from models import ingest_data
+        from mypackage import ingest_data
     except Exception as e:
         assert False, f"Error: {e}. ingest_data is not installed correctly."
 
     try:
-        from models import methods
+        from mypackage import methods
     except Exception as e:
-        assert False, f"Error: {e}. models is not installed correctly."
+        assert False, f"Error: {e}. methods is not installed correctly."
     try:
-        from models import score
+        from mypackage import score
     except Exception as e:
         assert False, f"Error: {e}. score is not installed correctly."
 
