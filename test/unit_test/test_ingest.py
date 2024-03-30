fa003ddfe1b5117bef8f307657c8be6a29acafb9
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-30 11:58:13 +0530

    created unit test which checks if data is being imported in ingest data

---
+++ test/unit_test/test_ingest.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+import pandas as pd
+
+from housingpriceprediction import ingest_data
+
+
+class test_data_ingestion(unittest.TestCase):
+    def test_fetch_housing_data(self):
+        ingest_data.fetch_housing_data()
+        self.assertTrue(os.path.exists("datasets/housing/housing.csv"))
