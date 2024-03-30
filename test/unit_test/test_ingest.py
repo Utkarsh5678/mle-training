389a9535e4fd1b90d1ca956badcb4792ffcb9477
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-30 12:08:03 +0530

    fixing path frm datasets to data

--- test/unit_test/test_ingest.py
+++ test/unit_test/test_ingest.py
@@ -8,4 +8,4 @@ from housingpriceprediction import ingest_data
 class test_data_ingestion(unittest.TestCase):
     def test_fetch_housing_data(self):
         ingest_data.fetch_housing_data()
-        self.assertTrue(os.path.exists("datasets/housing/housing.csv"))
+        self.assertTrue(os.path.exists("data/housing/housing.csv"))
