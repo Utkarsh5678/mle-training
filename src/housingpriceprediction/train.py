14833904857f47ce2d44b38a09b93b7d21461de7
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-03-31 12:59:28 +0530

    changed

--- src/housingpriceprediction/train.py
+++ src/housingpriceprediction/train.py
@@ -9,22 +9,12 @@ import os
 def train_linear_regression(X_train, y_train):
     lin_reg = LinearRegression()
     lin_reg.fit(X_train, y_train)
-    os.makedirs("artifacts", exist_ok=True)
-    model_filename = 'linear_regression.pkl'
-    model_path = os.path.join("artifacts", model_filename)
-    dump(lin_reg, "artifacts/linear_regression.pkl")
     return lin_reg
 
 
 def train_decision_tree(X_train, y_train):
     tree_reg = DecisionTreeRegressor(random_state=42)
     tree_reg.fit(X_train, y_train)
-    os.makedirs("artifacts", exist_ok=True)
-    model_filename = 'decision_tree.pkl'
-    model_path = os.path.join("artifacts", model_filename)
-    model_filename = 'linear_regression.pkl'
-    model_path = os.path.join("artifacts", model_filename)
-    dump(tree_reg, "artifacts/decision_tree.pkl")
     return tree_reg
 
 
@@ -44,10 +34,6 @@ def rand_tune_random_forest(X_train, y_train):
         random_state=42,
     )
     rnd_search.fit(X_train, y_train)
-    os.makedirs("artifacts", exist_ok=True)
-    model_filename = 'random_tune_random_forest.pkl'
-    model_path = os.path.join("artifacts", model_filename)
-    dump(rnd_search, "artifacts/random_tune_random_forest.pkl")
     return rnd_search
 
 
@@ -69,8 +55,4 @@ def grid_tune_random_forest(X_train, y_train):
         return_train_score=True,
     )
     grid_search.fit(X_train, y_train)
-    os.makedirs("artifacts", exist_ok=True)
-    model_filename = 'grid_tune_random_forest.pkl'
-    model_path = os.path.join("artifacts", model_filename)
-    dump(grid_search, "artifacts/grid_tune_random_forest.pkl")
     return grid_search
