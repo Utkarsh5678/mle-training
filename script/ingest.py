ec4f33b8ced8392cd764fe2f4086cd383d08e59f
Utkarsh5678 <utkarsh.ajmani@tigeranalytics.com>
2024-04-01 11:22:56 +0530

    Added code for logging in ingest.py which cretaes a file in the log folder and outputs data on the screen

--- script/ingest.py
+++ script/ingest.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import logging
+import sys
 
 from housingpriceprediction.ingest_data import (
     fetch_housing_data,
@@ -12,16 +14,35 @@ def save_csv(data, filename):
     data.to_csv(filename, index=False)
 
 def main(output_folder):
-    fetch_housing_data()
-    housing = load_housing_data()
-    X_train, X_test, y_train, y_test = prepare_data_for_training(housing)
+    # Create log file path in the root directory
+    log_file = os.path.join(os.getcwd(), "log", "housing_prediction.log")
+    
+    # Create log directory if it doesn't exist
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    
+    # Configure logging to both file and console
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(console_handler)
+    
+    try:
+        fetch_housing_data()
+        housing = load_housing_data()
+        X_train, X_test, y_train, y_test = prepare_data_for_training(housing)
 
-    os.makedirs(os.path.join(output_folder, "raw"), exist_ok=True)
-    os.makedirs(os.path.join(output_folder, "processed"), exist_ok=True)
+        os.makedirs(os.path.join(output_folder, "raw"), exist_ok=True)
+        os.makedirs(os.path.join(output_folder, "processed"), exist_ok=True)
 
-    save_csv(housing, os.path.join(output_folder, "raw", "housing.csv"))
-    save_csv(X_train, os.path.join(output_folder, "processed", "train_set.csv"))
-    save_csv(X_test, os.path.join(output_folder, "processed", "test_set.csv"))
+        save_csv(housing, os.path.join(output_folder, "raw", "housing.csv"))
+        save_csv(X_train, os.path.join(output_folder, "processed", "train_set.csv"))
+        save_csv(X_test, os.path.join(output_folder, "processed", "test_set.csv"))
+        
+        logging.info("Data processing and saving completed.")
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}", exc_info=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download and create stratified split datasets.")
